7e2f1240814e5a07069eb0f29769008d93362d4a
Esaurock <108309574+Esaurock@users.noreply.github.com>
2022-07-25 22:55:50 -0400

    Initial commit

---
+++ app/src/main/java/com/example/holaaaa/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.holaaaa;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    String b1,b2;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
